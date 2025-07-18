173c9b5f5b354e6b75406de38d61a682102181f3
amrayman1999 <amr.ayman.essawi.1999@gmail.com>
2025-07-18 21:35:56 +0300

    Question 4

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Assignment.Enums;
+using Demo.Enums;
 
 namespace Assignment
 {
@@ -14,37 +15,42 @@ namespace Assignment
             #endregion
 
             #region Question 2
-            bool isParsed;
-            object season;
-            do
-            {
-                Console.Write("Enter Season Name:");
-                isParsed = Enum.TryParse(typeof(Season), Console.ReadLine(), true, out season);
-                if (!isParsed)
-                {
-                    Console.WriteLine("Invalid input. Please enter a valid season name (Spring, Summer, Autumn, Winter).");
-                }
+            //bool isParsed;
+            //object season;
+            //do
+            //{
+            //    Console.Write("Enter Season Name:");
+            //    isParsed = Enum.TryParse(typeof(Season), Console.ReadLine(), true, out season);
+            //    if (!isParsed)
+            //    {
+            //        Console.WriteLine("Invalid input. Please enter a valid season name (Spring, Summer, Autumn, Winter).");
+            //    }
 
-            } while (!isParsed);
-            Season season1 = (Season)season;
-            switch (season1)
-            {
-                case Season.Spring:
-                    Console.WriteLine("Spring Range : March to May");
-                    break;
-                case Season.Summer:
-                    Console.WriteLine("Summer Range : March to MayJune to August");
-                    break;
-                case Season.Autumn:
-                    Console.WriteLine("Autumn Range : September to November");
-                    break;
-                case Season.Winter:
-                    Console.WriteLine("Winter Range : December to February");
-                    break;
-                default:
-                    Console.WriteLine("Unknown season.");
-                    break;
-            }
+            //} while (!isParsed);
+            //Season season1 = (Season)season;
+            //switch (season1)
+            //{
+            //    case Season.Spring:
+            //        Console.WriteLine("Spring Range : March to May");
+            //        break;
+            //    case Season.Summer:
+            //        Console.WriteLine("Summer Range : March to MayJune to August");
+            //        break;
+            //    case Season.Autumn:
+            //        Console.WriteLine("Autumn Range : September to November");
+            //        break;
+            //    case Season.Winter:
+            //        Console.WriteLine("Winter Range : December to February");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Unknown season.");
+            //        break;
+            //}
+            #endregion
+
+            #region Question 4
+            Permission permission = Permission.Read | Permission.Write | Permission.Execute | Permission.Delete;
+            Console.WriteLine($"Permissions: {permission}");
             #endregion
 
         }

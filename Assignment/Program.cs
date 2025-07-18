2d7eb2f1da1dea27124ed89cce92adf6d324d0c6
amrayman1999 <amr.ayman.essawi.1999@gmail.com>
2025-07-18 21:31:07 +0300

    Question 2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -7,11 +7,46 @@ namespace Assignment
         static void Main(string[] args)
         {
             #region Question 1
-            foreach (var day in Enum.GetNames(typeof(DayOfWeek)))
+            //foreach (var day in Enum.GetNames(typeof(DayOfWeek)))
+            //{
+            //    Console.WriteLine(day);
+            //}
+            #endregion
+
+            #region Question 2
+            bool isParsed;
+            object season;
+            do
             {
-                Console.WriteLine(day);
+                Console.Write("Enter Season Name:");
+                isParsed = Enum.TryParse(typeof(Season), Console.ReadLine(), true, out season);
+                if (!isParsed)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid season name (Spring, Summer, Autumn, Winter).");
+                }
+
+            } while (!isParsed);
+            Season season1 = (Season)season;
+            switch (season1)
+            {
+                case Season.Spring:
+                    Console.WriteLine("Spring Range : March to May");
+                    break;
+                case Season.Summer:
+                    Console.WriteLine("Summer Range : March to MayJune to August");
+                    break;
+                case Season.Autumn:
+                    Console.WriteLine("Autumn Range : September to November");
+                    break;
+                case Season.Winter:
+                    Console.WriteLine("Winter Range : December to February");
+                    break;
+                default:
+                    Console.WriteLine("Unknown season.");
+                    break;
             }
             #endregion
+
         }
     }
 }

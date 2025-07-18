36db750bcc6b0f60e6947d32232aed1bfa9f839f
amrayman1999 <amr.ayman.essawi.1999@gmail.com>
2025-07-18 21:21:39 +0300

    Question 1

---
+++ Assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using Assignment.Enums;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question 1
+            foreach (var day in Enum.GetNames(typeof(DayOfWeek)))
+            {
+                Console.WriteLine(day);
+            }
+            #endregion
+        }
+    }
+}
